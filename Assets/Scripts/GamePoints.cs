d3b6a2cf917cfd034ddcc0ea9bef11fa07b38295
LAPTOP-AK0LGJP3\mbarr <22300007@live.uem.es>
2024-04-26 21:08:51 -0400

    Unity Update 4.6

---
+++ Assets/Scripts/GamePoints.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GamePoints : MonoBehaviour
+{
+    public int points = 0;
+    public TextMeshProUGUI textPoints;
+
+
+}
