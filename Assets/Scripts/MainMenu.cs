1490cffdaa07223fad48d713372e6da0b2e25a94
LAPTOP-AK0LGJP3\mbarr <22300007@live.uem.es>
2024-06-14 13:23:50 -0400

    Unity Update 5.6

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CoinSound : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Jugar ()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
     // Update is called once per frame
-    void Update()
+    public void Salir ()
     {
-        
+        Application.Quit();
     }
 }
